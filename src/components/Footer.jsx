ff79d47022c249bf60efae9c086e39eb5eb8a34d
mohankumar01012005 <kumar.mohan@kalvium.community>
2024-04-27 13:59:41 +0530

    completed

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "../App.css"
+const Footer = () => {
+  return (
+    <div>
+        <div className="logoadd">
+           
+            <div>
+
+            <h1>Kalvium Store</h1>
+            <p>You have a new Course</p>
+            </div>
+        </div>
+            <div className='Footer'>© 2021 Copyright:Kalvium</div>
+    </div>
+  )
+}
+
+export default Footer
